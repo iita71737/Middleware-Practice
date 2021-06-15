e99925d91f9c111351adfab4e9211cfd409d29d0
danlin <iita71737@gmail.com>
2021-06-16 01:32:54 +0800

    added req res time log

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const app = express();
 const port = 3000;
 
 app.use(function (req, res, next) {
-  console.log(`${new Date()} | ${req.method}  from  ${req.url}`);
+  console.log(`Request Time : ${new Date()} | ${req.method}  from  ${req.url}`);
+  console.log('Request Time :', new Date())
   next();
 });
 
@@ -14,18 +15,20 @@ const logText = (req, res ,next) => {
 }
 
 app.get("/", (req, res) => {
-     res.send('列出全部 Todo')
+     res.status(400).send(new Date());
+     console.log(`Response Time : ${new Date()} `,new Date() );
+     console.log('Response Time:', new Date() );
 });
 
-app.get("/new", logText, (req, res) => {
+app.get("/new",  (req, res) => {
   res.send("新增 Todo 頁面");
 });
 
-app.get("/:id", logText,(req, res) => {
+app.get("/:id", (req, res) => {
   res.send("顯示一筆 Todo");
 });
 
-app.post("/", logText, (req, res) => {
+app.post("/",  (req, res) => {
   res.send("新增一筆  Todo");
 });
 
