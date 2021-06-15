a75683edd27aebba9bc8af21b3aaf157d08dc60c
danlin <iita71737@gmail.com>
2021-06-16 01:55:03 +0800

    revise console log

--- app.js
+++ app.js
@@ -3,21 +3,33 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   console.log(`Request Time : ${new Date()} | ${req.method}  from  ${req.url}`);
   console.log('Request Time :', new Date())
+  console.log('-------------------------------')
   next();
 });
 
-const logText = (req, res ,next) => {
-    console.log(`logfunc : ${new Date()} | ${req.method}  from  ${req.url}`);
-    next();
-}
+// const logText = (req, res ,next) => {
+//     let start = new Date();
+//      res.status(400).send(new Date());
+//      let end = new Date();
+//      let elapsed = end.getTime() - start.getTime() ;
+//      console.log(`Response Time : ${new Date()} `,new Date() );
+//      console.log('Response Time:', new Date() );
+//      console.log(`${new Date()} | ${req.method}  from  ${req.url} | total time: ${elapsed}`)
+//     next();
+// }
 
 app.get("/", (req, res) => {
+     let start = new Date();
      res.status(400).send(new Date());
+     let end = new Date();
+     let elapsed = end.getTime() - start.getTime() ;
      console.log(`Response Time : ${new Date()} `,new Date() );
      console.log('Response Time:', new Date() );
+     console.log(`${new Date()} | ${req.method}  from  ${req.url} | total time: ${elapsed}`)
+     console.log('-------------------------------')
 });
 
 app.get("/new",  (req, res) => {
